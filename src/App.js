3885cfb24ba188bcf7eb543e1b04dda8034491f1
Sa Eun Soo <tkdmstn0304@naver.com>
2020-02-04 16:39:43 +0900

    relode -v2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Popular from "./routes/Popular";
 import TopRate from "./routes/TopRate";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={window.location.pathname || ""}>
       <Route path="/" exact={true} component={Popular}></Route>
       <Route path="/popular" component={Popular}></Route>
       <Route path="/TopRate" exact={true} component={TopRate}></Route>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
